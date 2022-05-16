311943ba17c2f340dd470dde274e479338cf5a50
Sebastian Arneson <c21sebar@student.his.se>
2022-05-16 11:16:37 +0200

    Gjort klasser för att spara datan från json till ett Lake object och AuxData klassen finns som ett object i Lake klassen.

---
+++ app/src/main/java/com/example/project/AuxData.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+public class AuxData {
+    String wiki;
+    String img;
+
+    public String getWiki() {
+        return wiki;
+    }
+
+    public String getImg() {
+        return img;
+    }
+}
---
+++ app/src/main/java/com/example/project/Lake.java
@@ -0,0 +1,44 @@
+package com.example.project;
+
+public class Lake {
+    String name;
+    String type;
+    String company;
+    String location;
+    String category;
+    int size;
+    int cost;
+    AuxData auxdata;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public AuxData getAuxdata() {
+        return auxdata;
+    }
+}
