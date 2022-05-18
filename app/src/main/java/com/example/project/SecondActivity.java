12693274f6706e6eb4ae9a5497d337ce3dce1368
Sebastian Arneson <c21sebar@student.his.se>
2022-05-18 16:34:21 +0200

    Gjort en Second activity med xml som skan aktiveras när man klickar på ett item i recycler view och lagt till aktiviteten i AndroidManifest.xml

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,38 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+public class SecondActivity  extends AppCompatActivity {
+
+    TextView text;
+    Button button;
+    ImageView imageView;
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.second_activity);
+        super.onCreate(savedInstanceState);
+        final String name = getIntent().getExtras().getString("namn");
+        final String imgURL = getIntent().getExtras().getString("imgURL");
+
+        text =findViewById(R.id.secondActivityTextView);
+        text.setText(name);
+
+        imageView = findViewById(R.id.secondActivityImageView);
+        Picasso.get().load(imgURL.toString()).into(imageView);
+        button = findViewById(R.id.finishButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+}
